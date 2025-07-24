e529534e196d9e128bd0b3eed4e48826325d7dd7
Usama <usamakha2003@gmail.com>
2025-07-24 16:48:31 +0500

    finish project

--- react challenge/date counter/date-countter/src/App.js
+++ react challenge/date counter/date-countter/src/App.js
@@ -1,5 +1,46 @@
+import React, { useState } from "react";
 function App() {
-  return <div className="App">hello</div>;
+  const [step, setStep] = React.useState(1);
+  const [counter, setCounter] = React.useState(0);
+
+  const date = new Date();
+  const today = new Date(date);
+  today.setDate(date.getDate() + counter);
+  const dates = today.toDateString();
+
+  const centerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  return (
+    <div className="App">
+      <div style={centerStyle}>
+        <button onClick={step < 10 ? () => setStep(step + 1) : undefined}>
+          +
+        </button>
+        <p>Step: {step}</p>
+        <button onClick={step > 1 ? () => setStep(step - 1) : undefined}>
+          -
+        </button>
+      </div>
+      <div style={centerStyle}>
+        <button
+          onClick={counter < 365 ? () => setCounter(counter + step) : undefined}
+        >
+          +
+        </button>
+        <p>Conter: {counter}</p>
+        <button onClick={() => setCounter(counter - 1)}>-</button>
+      </div>
+
+      <div style={centerStyle}>
+        <p>
+          {counter} day from is {dates}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
