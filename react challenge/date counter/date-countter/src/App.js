90cecdee00ba75204de7eac09752f8273bca5f2a
Usama <usamakha2003@gmail.com>
2025-07-29 01:02:13 +0500

    update project

--- react challenge/date counter/date-countter/src/App.js
+++ react challenge/date counter/date-countter/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 function App() {
-  const [step, setStep] = React.useState(1);
-  const [counter, setCounter] = React.useState(0);
+  const [step, setStep] = useState(1);
+
+  const [inputValue, setInputValue] = useState(0);
 
   const date = new Date();
   const today = new Date(date);
-  today.setDate(date.getDate() + counter);
+  today.setDate(date.getDate() + inputValue);
   const dates = today.toDateString();
 
   const centerStyle = {
@@ -16,28 +17,44 @@ function App() {
   return (
     <div className="App">
       <div style={centerStyle}>
-        <button onClick={step < 10 ? () => setStep(step + 1) : undefined}>
-          +
-        </button>
-        <p>Step: {step}</p>
-        <button onClick={step > 1 ? () => setStep(step - 1) : undefined}>
-          -
-        </button>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
+        <p>{step}</p>
       </div>
       <div style={centerStyle}>
+        <button onClick={() => setInputValue(inputValue + step)}>+</button>
+        <input
+          type="number"
+          value={inputValue}
+          onChange={(e) => setInputValue(+e.target.value)}
+        />
         <button
-          onClick={counter < 365 ? () => setCounter(counter + step) : undefined}
+          onClick={() => setInputValue(inputValue - step)}
+          disabled={inputValue - step < 0}
         >
-          +
+          -
         </button>
-        <p>Conter: {counter}</p>
-        <button onClick={() => setCounter(counter - 1)}>-</button>
       </div>
 
       <div style={centerStyle}>
         <p>
-          {counter} day from is {dates}
+          {inputValue} day from is {dates}
         </p>
+      </div>
+      <div style={centerStyle}>
+        <button
+          onClick={() => {
+            setStep(1);
+            setInputValue(0);
+          }}
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
